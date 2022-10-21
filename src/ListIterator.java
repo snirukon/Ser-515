5a315c981a8a3ce937980fd5749e3e0dd0dbab98
snirukon <snirukon@asu.edu>
2022-10-20 22:29:36 -0700

    listiterator initiated

--- src/ListIterator.java
+++ src/ListIterator.java
@@ -1,4 +1,4 @@
-//Gnana Chaitanya Ummadisingu -1222995072//
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.ArrayList;
@@ -9,9 +9,26 @@ public class ListIterator {
 
     public static Iterator<Integer> range(int start, int end) {
         return new Iterator<>() {
-
+            private int i = start;
+            @Override
+            public boolean hasNext() {
+                return i < end;
+            }
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return i++;
+            }
         };
     }
 
-
+    public static void iterate(ArrayList<String> pname) {
+        System.out.println("Menu");
+        var iterator = range(0, pname.toArray().length);
+        while (iterator.hasNext()) {
+            System.out.println(pname.get(iterator.next()));
+        }
+    }
 }
